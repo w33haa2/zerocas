c20473220df775338c40c7325a3779b68023463b
Jan Rey Suarez <w33haa2@gmail.com>
2020-06-02 09:06:07 +0800

    janrey > update

--- config/database.php
+++ config/database.php
@@ -62,14 +62,14 @@ return [
         ],
 
         'pgsql' => [
-             'driver'   => 'pgsql',
-             'host'     => $host,
-             'database' => $database,
-             'username' => $username,
-           'password' => $password,
-           'charset'  => 'utf8',
-           'prefix'   => '',
-           'schema'   => 'public',
+            'driver'   => 'pgsql',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
         ],
 
         'sqlsrv' => [
